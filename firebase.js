b3c5312c317ca642e0215bb970fdb740ce7c986b
Kelly Ye <kellyye@berkeley.edu>
2021-12-04 11:39:17 -0800

    initial commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA-9HxbIR7GNGOnJmmtVVGieTotJabnQEQ",
+    authDomain: "tts-login-page.firebaseapp.com",
+    projectId: "tts-login-page",
+    storageBucket: "tts-login-page.appspot.com",
+    messagingSenderId: "549449330093",
+    appId: "1:549449330093:web:d1ba4347ee2950c89fc524",
+    measurementId: "G-F2XYNV7WJL"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
